57e9696692db71e6d2687ab12cf82a0ae4265512
Martin Hamant (OW2) <mh@ow2.org>
2019-08-05 12:14:15 +0000

    OMM loop and restore_db runtime flag

--- dashboard/importer-omm-metrics.sh
+++ dashboard/importer-omm-metrics.sh
@@ -3,8 +3,23 @@
 # Import all the OMM metrics to Elasticsearch to be used in Kibana
 cd scava-metrics
 
-if [[ $DASHDEBUG -eq 1 ]]; then
-    ./omm2es.py -g -u https://gist.githubusercontent.com/valeriocos/1d5215d6ea212d3ecfc909492aad656e/raw/8910fb62de9f29aa5f6fe4e288f923b9af81a336/gistfile1.txt -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
-else
-    ./omm2es.py -u https://gist.githubusercontent.com/valeriocos/1d5215d6ea212d3ecfc909492aad656e/raw/8910fb62de9f29aa5f6fe4e288f923b9af81a336/gistfile1.txt -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
-fi
+OMM_CSV_LIST='
+https://projects.ow2.org/download/docdoku/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/clif/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/spoon/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/bonita/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/xwiki/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/knowage/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/proactive/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/asm/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/lutece/WebHome/ommv3-2018.csv
+https://projects.ow2.org/download/sat4j/WebHome/ommv3-2018.csv
+'
+
+for CSV in ${OMM_CSV_LIST}; do
+	if [[ $DASHDEBUG -eq 1 ]]; then
+		./omm2es.py -g -u ${CSV} -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
+	else
+		./omm2es.py -u ${CSV} -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
+	fi
+done
--- oss-db/mongo-restore.sh
+++ oss-db/mongo-restore.sh
@@ -9,37 +9,41 @@ chmod 777 /var/log/mongodb.log
 echo "Starting DB repair"
 /entrypoint.sh mongod --repair --logpath /var/log/mongodb.log --logappend --port 8001
 
-echo "Starting restore session"
+if [ $RESTORE_DB -eq 1 ]; then
 
-# Start mongodb with logging
-# --logpath    Without this mongod will output all log information to the standard output.
-# --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
-/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend --port 8000 &
+	echo "Starting restore session"
+
+	# Start mongodb with logging
+	# --logpath    Without this mongod will output all log information to the standard output.
+	# --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
+	/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend --port 8000 &
 
 
-# Wait until mongo logs that it's ready (or timeout after 60s)
-COUNTER=0
-grep -q 'waiting for connections on port' /var/log/mongodb.log
-while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
-    sleep 2
-    let COUNTER+=2
-    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
-    grep -q 'waiting for connections on port' /var/log/mongodb.log
-done
+	# Wait until mongo logs that it's ready (or timeout after 60s)
+	COUNTER=0
+	grep -q 'waiting for connections on port' /var/log/mongodb.log
+	while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
+	    sleep 2
+	    let COUNTER+=2
+	    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+	    grep -q 'waiting for connections on port' /var/log/mongodb.log
+	done
 
-# Restore from dump
-shopt -s nullglob
-DUMPS=(*20190715*.gz)
-NB_DUMPS=${#DUMPS[@]}
+	# Restore from dump
+	shopt -s nullglob
+	DUMPS=(*20190715*.gz)
+	NB_DUMPS=${#DUMPS[@]}
 
-DCOUNT=0
-for DUMP in "${DUMPS[@]}" ; do
-        ((DCOUNT++))
-        echo "Restoring (${DCOUNT}/${NB_DUMPS})" && mongorestore  --port 8000 --gzip --quiet --archive=${DUMP}
-done
+	DCOUNT=0
+	for DUMP in "${DUMPS[@]}" ; do
+		((DCOUNT++))
+		echo "Restoring (${DCOUNT}/${NB_DUMPS})" && mongorestore  --port 8000 --gzip --quiet --archive=${DUMP}
+	done
 
-/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend --port 8000 --shutdown &&
-echo "Restore session closed" &&
+	/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend --port 8000 --shutdown &&
+	echo "Restore session closed" || exit
+
+fi
 
 echo "Starting listening session on port 27017" &&
 /entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend &&
