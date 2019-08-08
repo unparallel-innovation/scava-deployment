6c3871f47bd7891e5a6d31601b446ca58a6b8028
Martin Hamant (OW2) <mh@ow2.org>
2019-08-08 20:40:12 +0000

    new way to proceed with scancode

--- dashboard/importer-scancode-metrics.sh
+++ dashboard/importer-scancode-metrics.sh
@@ -5,24 +5,34 @@ cd scava-metrics
 
 DATA_TIMESTAMP='2018-01-01'
 
-PROJECT_LIST='
-docdoku
-clif
-spoon
-bonita
-xwiki
-knowage
-proactive
-asm
-lutece
-sat4j
+# project names mapping
+# scancode json output filename in GitLab CI pattern is scancode-<gitreponame>.json
+# OW2 : SCAVA : Git
+PMAP='
+docdoku:docdokuplm:docdoku-plm
+clif:cliflegacy:clif-legacy
+spoon:spoon:spoon
+bonita:BonitaStudio:bonita-studio
+xwiki:XWiki:xwiki-platform
+knowage:KnowageServer:Knowage-Server
+proactive:scheduling:scheduling
+asm:asm:asm
+lutece:Lutece:lutece-core
+sat4j:sat4j:sat4j
 '
 
-for PROJECT in ${PROJECT_LIST}; do
-	URL="https://gitlab.ow2.org/ow2/oscar/${PROJECT}/-/jobs/artifacts/master/raw/scancode-${PROJECT}.json?job=scancode"
+for P in ${PMAP}; do
+	IFS=':' read POW2 PSCAVA PGIT <<< "${P}"
+	echo ----${POW2}
+	echo ----${PSCAVA}
+	echo ----${PGIT}
+	# latest artifacts can be browsed with https://gitlab.ow2.org/ow2/<PROJECT>/asm/-/jobs/artifacts/master/browse?job=scancode
+
+	URL="https://gitlab.ow2.org/ow2/oscar/${POW2}/-/jobs/artifacts/master/raw/scancode-${PGIT}.json?job=scancode"
+	echo "Now importing data to ES from ${URL} and to SCAVA project ${POW2}"
 	if [[ $DASHDEBUG -eq 1 ]]; then
-		./scancode2es.py -g -t ${DATA_TIMESTAMP} -u ${URL} -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
+		./scancode2es.py -g -p ${PSCAVA} -t ${DATA_TIMESTAMP} -u ${URL} -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
 	else
-		./scancode2es.py -t ${DATA_TIMESTAMP} -u ${URL} -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
+		./scancode2es.py -p ${PSCAVA} -t ${DATA_TIMESTAMP} -u ${URL} -e https://admin:admin@elasticsearch:9200 -i scava-metrics --bulk-size $BULKSIZE;
 	fi
 done
